4021904a2e05d010cd116bfb4cf3dadf6ab6e910
tangke <406517780@qq.com>
2017-07-24 17:16:58 +0800

    1

--- zeromq_learn/zeromq_learn.cpp
+++ zeromq_learn/zeromq_learn.cpp
@@ -17,8 +17,21 @@ int main()
 		zmq_msg_t request;
 		zmq_msg_init(&request);
 		zmq_msg_recv(&request, responder, 0);
+		printf("Received Hello\n");
+		zmq_msg_close(&request);
+
+		//do something
+		Sleep(1);
+
+		zmq_msg_t reply;
+		zmq_msg_init_size(&reply, 5);
+		memcpy(zmq_msg_data(&reply), "world", 5);
+		zmq_msg_send(&reply, responder, 0);
+		zmq_msg_close(&reply);
 
 	}
+	zmq_close(responder);
+	zmq_ctx_destroy(&context);
 	return 0;
 }
 
